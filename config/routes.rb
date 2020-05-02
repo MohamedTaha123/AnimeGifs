a81cc3c94ba9933ce7470b7e27b4b1b110e3c9a9
taha <mohamedtaha.boutayeb@gmail.com>
2020-05-02 07:16:22 +0000

    chore: minor changes on carrierwave

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,6 @@ Rails.application.routes.draw do
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  root to: 'home#index'
+  root to: 'gifs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
