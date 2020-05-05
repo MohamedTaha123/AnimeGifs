fd7613321f027c72669d4a4596c19a54cba572e2
taha <mohamedtaha.boutayeb@gmail.com>
2020-05-05 04:10:06 +0000

    fix: fix public activity model owner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :masquerade_user!
   before_action :set_notifications, if: :user_signed_in?
-
+  include PublicActivity::StoreController
  
   protected
 
