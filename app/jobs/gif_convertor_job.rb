728bfe60d75e6123533bf8277c4ed918635e2e50
taha <mohamedtaha.boutayeb@gmail.com>
2020-06-28 01:28:33 +0100

    chore: minor changes

--- app/jobs/gif_convertor_job.rb
+++ app/jobs/gif_convertor_job.rb
@@ -6,20 +6,19 @@ class GifConvertorJob < ApplicationJob
   require 'skeptick'
   include Skeptick
   def perform(nwi, current_user)
-    # MiniMagik::Image.convert -delay 10 -loop 0 browse_images(nwi.images_hash) 'anim.gif'
     # Skeptick & MiniMagick  Api
-    puts browse_images(nwi.images_hash)
     new_gif = Rails.public_path.join('uploads','gif', "new_gif_#{nwi.id}.gif")
     command = convert(browse_images(nwi.images_hash)) do
-      set :delay, '200'
+      set :delay, '5x100'
       set :loop, '0'
       write new_gif
     end
     command.run
-    puts new_gif.to_path
     Gif.new(id: nwi.gif_id, image: URI.parse(new_gif.to_path), user_id: current_user.id,
                 tag_list: %w[1 2 3],
                 description: nwi.description, label: nwi.label).save!
+  rescue StandardError => e 
+    puts e.to_s
   end
 
   def browse_images(images)
