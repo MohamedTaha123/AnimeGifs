4f3799ea83c4c172cb5b45234463de8aa31f9167
taha <mohamedtaha.boutayeb@gmail.com>
2020-07-22 21:31:44 +0100

    chore: use redis cache on development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
   # config.cache_store = :redis_cache_store, {url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" }}
-  config.session_store :cache_store
+  
  
 
   # In the development environment your application's code is reloaded on
@@ -23,16 +23,16 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    DEFAULT_EXPIRATION = 1.hour.to_i.freeze
+    config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL"], expires_in: DEFAULT_EXPIRATION }
+
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-    # config.action_controller.perform_caching = true
 
     config.cache_store = :null_store
-    ## Redis cache store
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
