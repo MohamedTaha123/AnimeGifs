69c20ed8e9af8f52b471efc3d9a11052c842d70d
taha <mohamedtaha.boutayeb@gmail.com>
2020-07-24 18:46:37 +0100

    chore: minor changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,9 +24,10 @@ Rails.application.configure do
 
     DEFAULT_EXPIRATION = 1.hour.to_i.freeze
     DEFAULT_CACHE = 'cache'
-    config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL"]
-                  , namespace: DEFAULT_CACHE,
-                   expires_in: DEFAULT_EXPIRATION }
+    config.cache_store = :redis_cache_store, { 
+                  url: ENV["REDIS_URL"],
+                  namespace: DEFAULT_CACHE,
+                  expires_in: DEFAULT_EXPIRATION }
 
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
