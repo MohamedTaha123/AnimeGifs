c866ad6cdc74554b9e8fc8f621929678ce5003b7
taha <mohamedtaha.boutayeb@gmail.com>
2020-09-10 15:24:10 +0100

    chore: remove exception handler

--- config/application.rb
+++ config/application.rb
@@ -24,43 +24,6 @@ module MyApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
-    # Exception Handler
-    config.exception_handler =
-      { dev: false,
-        db: nil,
-        # Custom Exceptions
-        custom_exceptions: {
-          # 'ActionController::RoutingError' => :not_found # => example
-        },
-
-        # On default 5xx error page, social media links included
-        social: {
-          facebook: nil, # Facebook page name
-          twitter: nil, # Twitter handle
-          youtube: nil, # Youtube channel name / ID
-          linkedin: nil, # LinkedIn name
-          fusion: nil # FL Fusion handle
-        },
-        exceptions: {
-
-          :all => {
-            layout: 'exception', # define layout
-            notification: true # (false by default)
-            # action: ____, (this is general)
-            # background: (can define custom background for exceptions layout if required)
-          },
-          404 => {
-            layout: 'exception', # define layout
-            notification: true # (false by default)
-            # action: ____, (this is general)
-            # background: (can define custom background for exceptions layout if required)
-          },
-          500 => {
-            layout: 'exception', # define layout
-            notification: true # (false by default)
-            # action: ____, (this is general)
-            # background: (can define custom background for exceptions layout if required)
-          }
-        } }
+    
   end
 end
