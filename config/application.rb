d61daba0ef909323055a11a8271f4b73d175ed67
taha <mohamedtaha.boutayeb@gmail.com>
2020-09-10 01:58:56 +0100

    fix: uninitialized constant Dotenv while pushing rails app to heroku

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,11 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+# FIXME: fix it for heroku deployment
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end 
+
 module MyApi
   class Application < Rails::Application
     # Expose our application's helpers to Administrate
@@ -15,6 +19,7 @@ module MyApi
       Administrate::ApplicationController.helper MyApi::Application.helpers
     end
     config.active_job.queue_adapter = :sidekiq
+
     config.application_name = Rails.application.class.module_parent_name
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
